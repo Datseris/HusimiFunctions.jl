03b0af38821d62e98f77031eb975770cf2c5a8c0
George Datseris <datseris.george@gmail.com>
2019-12-20 13:21:21 +0100

    Improve the core husimi code a bit

--- src/husimi.jl
+++ src/husimi.jl
@@ -1,67 +1,43 @@
+export husimi
+
 using LinearAlgebra # for ⋅ (cdot)
 
 struct Zero end
 Base.:+(x, ::Zero) = x
-Base.convert(Float64, ::Zero) = 0.0
+Base.convert(::Type{Float64}, ::Zero) = 0.0
+Base.convert(::Type{Any}, ::HusimiFunctions.Zero) = 0.0
 
 const VSV{D} = Vector{SVector{D, Float64}}
 
-struct Husimi{D}
-    Q::Matrix{Float32}
-    r0s::VSV{D}
-    k0s::VSV{D}
-    σ::Float64
-    B::Float64
-end
-
-Husimi(Q::Matrix, r, k, σ) = Husimi(Q, r, k, σ, 0)
-Husimi(Q::Matrix, r::VSV{D}, k, σ, B) where {D} = Husimi{D}(Q, r, k, σ, B)
-
 """
-    Husimi(ψ::Vector{Complex}, r, r0s, k0s, σ, [B])
-Calculate a `Husimi` function at positions `r0s` and wavevectors `k0s`,
-for the wavefunction `ψ`. The resulting struct has the following fields:
-```julia
-Q, r0s, k0s, σ, B
-```
-where `Q[i,j]` is measured at `r0s[i], k0s[j]`
-*notice that `ψ` is not stored*.
-
-`r, r0s, k0s` must be vectors of `SVector`s.
-
-    Husimi(Q::Matrix, r, r0s, k0s, σ, [B])
-Directly instantiate the `Husimi` container with a pre-calculated `Q`.
-"""
-function Husimi(ψ, r, r0s::VSV{D}, k0s::VSV{D}, σ, B = Zero();
-    views = [todo for i in 1:length(r0s)]) where {D}
-    Q = zeros(Float32, length(r0s), length(k0s))
-    for j in 1:length(k0s)
-        for i in 1:length(r0s)
-            @inbounds Q[i,j] = husimi(ψ, r, r0s[i], k0s[j])
-        end
-    end
-    return Husimi{D}(Q, r0s, k0s, σ, B)
-end
-
-"""
-    husimi(ψ, r::Vector{<:SVector}, r₀::SVector, k₀::SVector, σ[, B])
+    husimi(ψ, r::Vector{<:SVector}, r₀::SVector, k₀::SVector, σ[, B]; thread)
 Calculate the Husimi function of `ψ` at `(r₀, k₀)` with precision `σ`.
 `ψ` is a vector whose values `ψ[i]` is the wavefunction value
 at position `r[i]` (`r` is a vector of `SVector`s).
+The keyword `thread` means to parallelize the operation over processor threads
+(`true` by default).
 
 Optionally calculate in a non-zero magnetic field `B`.
-
 The units of `r, σ` are expected in nm, `k` in 1/nm and B in Tesla.
+
+Notice that the magnetic transformation assumes Landau gauge in x orientation,
+see https://arxiv.org/abs/1912.04622 for defining other gauges (by changing
+the function `magneticphase`).
 """
-function husimi(ψ, r, r₀::SVector, k₀::SVector, σ, B = Zero())
-    abs2(coherent_projection(ψ, r, r₀, k₀, σ, B))
+function husimi(ψ, r, r₀::SVector, k₀::SVector, σ, B = Zero();
+                thread = true)
+    if thread
+        abs2(coherent_projection_threaded(ψ, r, r₀, k₀, σ, B))
+    else
+        abs2(coherent_projection(ψ, r, r₀, k₀, σ, B))
+    end
 end
 
 ##############################################################
 # Projection
 ##############################################################
 # TODO: Check if dot is as optimized as  r[1]*r[1] + r[2]*r[2]
-function coherent_projection(ψ, r, r₀, k₀, σ, B = Zero())
+function coherent_projection_threaded(ψ, r, r₀, k₀, σ, B = Zero())
     D = length(r₀)
     sums = zeros(ComplexF64, Threads.nthreads())
     skip = (3*σ)^2
@@ -71,7 +47,6 @@ function coherent_projection(ψ, r, r₀, k₀, σ, B = Zero())
         δr = r[i] - r₀
         rsq = δr⋅δr
         rsq > skip && continue
-        # Projection to coherent state:
         space = exp(-rsq/denominator)
         phase = k₀⋅δr
         mphase = magneticphase(r[i], r₀, B)
@@ -79,6 +54,25 @@ function coherent_projection(ψ, r, r₀, k₀, σ, B = Zero())
         sums[Threads.threadid()] += conj(ψ[i]) * space * complex(cs, ss)
     end
     return sum(sums)/(σ*sqrt(2π))^(D/2)
+end
+
+function coherent_projection(ψ, r, r₀, k₀, σ, B = Zero())
+    D = length(r₀)
+    sums = zero(ComplexF64)
+    skip = (3*σ)^2
+    denominator = (4*σ^2)
+
+    @inbounds @fastmath for i in 1:length(r)
+        δr = r[i] - r₀
+        rsq = δr⋅δr
+        rsq > skip && continue
+        space = exp(-rsq/denominator)
+        phase = k₀⋅δr
+        mphase = magneticphase(r[i], r₀, B)
+        ss, cs = sincos(phase + mphase)
+        sums += conj(ψ[i]) * space * complex(cs, ss)
+    end
+    return sums/(σ*sqrt(2π))^(D/2)
 end
 
 const prefac = (10.0^(-2.0))/6.582 # e/ħ prefactor, when multiplying Tesla gives 1/nm^2
